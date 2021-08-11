6a9de8d8dbb97a260357b85379659b056265039f
cpan1995 <cpan1995@gmail.com>
2021-08-11 18:33:49 -0400

    commit tree 5d87e1cba8be783652dff328c4804d3b34631cac

---
+++ lib/user.rb
@@ -0,0 +1,14 @@
+class User
+
+    attr_accessor :first_name, :last_name
+
+    def first_name=(value)
+        @first_name = value
+    end 
+
+    def last_name=(value)
+        @last_name = value
+    end 
+
+end
+
